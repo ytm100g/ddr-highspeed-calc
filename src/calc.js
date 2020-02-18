2a534a7b6c141f28e7adfc7265327c2bdb36947d
ytm100g <ytm100g@gmail.com>
2020-02-18 23:41:08 +0000

    テスト用のファイルを作成

--- src/calc.js
+++ src/calc.js
@@ -1,9 +1,9 @@
 const MODIFY_NUMBERS = [
   0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0,
   3.25, 3.5, 3.75, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0
-];
+]; // https://p.eagate.573.jp/game/ddr/ddra20/p/howto/option_list.html
 
-function calcModyfyNumber(fitSpeed, bpm) {
+function calcModyfyNumber (fitSpeed, bpm) {
   let i = 0;
   while (fitSpeed > bpm * MODIFY_NUMBERS[i]) {
     i++;
@@ -22,3 +22,7 @@ function calcModyfyNumber(fitSpeed, bpm) {
   }
   return result;
 }
+
+module.exports = {
+  calcModyfyNumber: calcModyfyNumber
+};
---
+++ test/calc.test.js
@@ -0,0 +1,5 @@
+const mocha = require("mocha");
+const chai = require("chai");
+const calc = require("../src/calc"); // Test Target
+
+console.log(calc.calcModyfyNumber(600, 200));
